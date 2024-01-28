3bdf46dae2c293cd0b952d960fd32a97b4e598ad
delimmaemma <calvinhtml@gmail.com>
2024-01-28 13:30:50 -0600

    Changed structure of app.py and implemented public routes

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
+from models.nosql_models import db, Affirmation
+from helpers.nosql_helpers import get_public_affirmations, add_public_affirmation, update_public_affirmation, delete_public_affirmation, get_public_affirmations_by_category, get_public_affirmations_by_keyword, get_public_affirmations_by_category_and_keyword, get_public_affirmations_by_id
 
 app = Flask(__name__)
+app.config['MONGODB_SETTINGS'] = {
+    'db': 'affirmations',
+    'host': 'localhost'
+}
+
+db.init_app(app)
 
 @app.route('/affirmations', methods=['GET'])
 def affirmations():
@@ -18,45 +26,71 @@ def affirmations_put():
 def affirmations_delete():
     return jsonify({'message': 'This is the delete route for affirmations'})
 
-@app.route('/affirmations/global', methods=['GET'])
-def affirmations_global():
-    return jsonify({'message': 'This is the get route for global affirmations'})
-
-@app.route('/affirmations/global', methods=['POST'])  # Used only if the user selects to have their affirmation uploaded publicly
-def affirmations_global_post():
-    return jsonify({'message': 'This is the post route for global affirmations'})
-
-@app.route('/affirmations/global', methods=['PUT'])  # Might change to patch
-def affirmations_global_put():
-    return jsonify({'message': 'This is the put route for global affirmations'})
-
-@app.route('/affirmations/global', methods=['DELETE'])
-def affirmations_global_delete():
-    return jsonify({'message': 'This is the delete route for global affirmations'})
-
 @app.route('/affirmations/<category>', methods=['GET'])
 def affirmations_category(category):
     return jsonify({'message': f'This is the get route for the affirmations {category} category'})
-
-@app.route('/affirmations/global/<category>', methods=['GET'])
-def affirmations_global_category(category):
-    return jsonify({'message': f'This is the get route for the global affirmations {category} category'})
 
 @app.route('/affirmations/<keyword>', methods=['GET'])
 def affirmations_keyword(keyword):
     return jsonify({'message': f'This is the get route for the affirmations {keyword} keyword'})
 
-@app.route('/affirmations/global/<keyword>', methods=['GET'])
-def affirmations_global_keyword(keyword):
-    return jsonify({'message': f'This is the get route for the global affirmations {keyword} keyword'})
-
 @app.route('/affirmations/<category>/<keyword>', methods=['GET'])
 def affirmations_category_keyword(category, keyword):
     return jsonify({'message': f'This is the get route for the affirmations {category} category and {keyword} keyword'})
 
-@app.route('/affirmations/global/<category>/<keyword>', methods=['GET'])
-def affirmations_global_category_keyword(category, keyword):
-    return jsonify({'message': f'This is the get route for the global affirmations {category} category and {keyword} keyword'})
+@app.route('/affirmations/public', methods=['GET'])
+def affirmations_public():
+    affirmations = get_public_affirmations()
+    return jsonify(affirmations)
+
+@app.route('/affirmations/public', methods=['POST'])  # Used only if the user selects to have their affirmation uploaded publicly
+def affirmations_public_post():
+    data = request.json
+    add_public_affirmation(data['user'], data['user_id'], data['category'], data['keyword'], data['affirmation_text'])
+
+@app.route('/affirmations/public', methods=['PUT'])  # Might change to patch
+def affirmations_public_put():
+    data = request.json
+    affirmation_id = data.get('id')
+    if not affirmation_id:
+        return jsonify({'message': 'Missing affirmation ID in the request'}), 400
+
+    updated_affirmation = update_public_affirmation(
+        affirmation_id,
+        data.get('category'),
+        data.get('keyword'),
+        data.get('affirmation_text')
+    )
+
+    if updated_affirmation:
+        return jsonify({'message': 'Public affirmation updated successfully'})
+    else:
+        return jsonify({'message': 'Public affirmation not found'}), 404
+
+@app.route('/affirmations/public', methods=['DELETE'])
+def affirmations_public_delete():
+    data = request.json
+    affirmation_id = data.get('id')
+    if not affirmation_id:
+        return jsonify({'message': 'Missing affirmation ID in the request'}), 400
+
+    result = delete_public_affirmation(affirmation_id)
+    return jsonify(result)
+
+@app.route('/affirmations/public/<category>', methods=['GET'])
+def affirmations_public_category(category):
+    affirmations = get_public_affirmations_by_category(category)
+    return jsonify(affirmations)
+
+@app.route('/affirmations/public/<keyword>', methods=['GET'])
+def affirmations_public_keyword(keyword):
+    affirmations = get_public_affirmations_by_keyword(keyword)
+    return jsonify(affirmations)
+
+@app.route('/affirmations/public/<category>/<keyword>', methods=['GET'])
+def affirmations_public_category_keyword(category, keyword):
+    affirmations = get_public_affirmations_by_category_and_keyword(category, keyword)
+    return jsonify(affirmations)
 
 if __name__ == '__main__':
     app.run(debug=True)
